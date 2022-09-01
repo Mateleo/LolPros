068b52e817f68bd27a27422abd3f6eabb157eb0b
Martin Gayet <63463668+Turdyo@users.noreply.github.com>
2022-09-02 00:40:11 +0200

    Update wsgi.py

--- backend/lolpros/lolpros/wsgi.py
+++ backend/lolpros/lolpros/wsgi.py
@@ -13,4 +13,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lolpros.settings")
 
-application_test = get_wsgi_application()
+application = get_wsgi_application()
